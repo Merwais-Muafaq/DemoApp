0d3f43851c641c6e1da85599de03f14214696a99
Merwais-Muafaq <40802512+Merwais-Muafaq@users.noreply.github.com>
2018-07-14 16:35:01 +0530

    Update javascript.js

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -54,3 +54,30 @@ $(document).ready(function() {
     
     
     });
+
+
+    //it will work fo a tag
+    // $(document).ready(function() {
+    //     $("a[href='#top']").click(function() {
+    //         $("html, body").animate({ scrollTop: 0 }, "slow");
+    //         return false;
+    //     });
+    
+    //     $(window).scroll(function(){
+    //          if ($(this).scrollTop() > 200) {
+    //              $('.btn-scroll-to-top').fadeIn();
+    //          } else {
+    //              $('.btn-scroll-to-top').fadeOut();
+    //          }
+    //     });
+    // });
+
+
+  $(function() {
+   $("#Registerfrmzab").click(function()
+    {
+       $("#rgsfrmzab").animate( {
+          height:'toggle'
+       });
+    });
+  });
